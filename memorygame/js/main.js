3d6cd0eba96bc20a26dcbe78defe45ef7932e788
Mohammed Asslahi <asslahi37@gmail.com>
2019-05-27 00:57:11 +0300

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,9 +1,25 @@
+
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen","queen","king","king"]
+var cardInPlay = []
+
+var cardOne = cards[0]
+cardInPlay.push(cardOne)
+
+var cardTwo = cards[2]
+cardInPlay.push(cardTwo)
+
+
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+cardInPlay.length === 4;
+
+if (cardInPlay[0] === cardInPlay[1]) {
+  alert("You found a match!");
+} else {
+    alert("sorry, try again.");
+
+}
