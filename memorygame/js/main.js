43edb760dec637cc75af3bf3a4cfa0a8559a1ee3
Mohammed Asslahi <asslahi37@gmail.com>
2019-05-30 03:03:21 +0300

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,7 +1,33 @@
 
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"]
+var cards = [
+{
+rank: 'queen',
+suit: 'heart',
+cardImage: "images/queen-of-hearts.png"
+},
+
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
 var cardsInPlay = []
 
 
@@ -16,16 +42,18 @@ function checkForMatch() {
 
 function flipCard (cardId) {
 
-console.log("user flipped" + " " + cards[cardId])
-cardsInPlay.push(cards[cardId])
+console.log("user fliphped" + " " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+cardsInPlay.push(cards[cardId].rank);
 
 
 cardsInPlay.length === 2;
-
-checkForMatch();
 
 }
 
 flipCard(0);
 flipCard(2);
-
+// console.log(cards.cardImage);
+// console.log(cards.suit);
